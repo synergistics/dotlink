4e91c0f60d2daf6f962ce656acf26ad44aa2258f
synergistics <kingjak678@gmail.com>
2015-09-27 12:22:21 -0400

    Adds minimal show command. Add options to it for raw output and other things later

--- dotlink/relevancedict.py
+++ dotlink/relevancedict.py
@@ -1,22 +1,27 @@
 from scripts.init import init
 from scripts.link import link
 from scripts.rmlink import rmlink
+from scripts.show import show 
 # from scripts.migrate import migrate
 
 relevance_dict = {
     "init": {
         "relevants_model": ("<path>"),
         "func": init
-     },
+        },
 
     "link": {
         "relevants_model": ("-s", "<target>", "<link_name>"),
         "func": link
-    },
+        },
 
     "rmlink": {
         "relevants_model": ("<link_name>"),
         "func": rmlink
-    }
-}
+        },
 
+    "show": {
+        "relevants_model": (),
+        "func": show
+        }
+    }
