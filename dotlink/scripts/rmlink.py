56d1679bdaf476d110aea6c2130d9691f4a5a81e
synergistics <kingjak678@gmail.com>
2015-09-27 12:21:52 -0400

    Makes dotlink check before removing link properly in two steps

--- dotlink/scripts/rmlink.py
+++ dotlink/scripts/rmlink.py
@@ -10,18 +10,22 @@ def rmlink(command_relevants):
     dotlink_dir = get_dotlink_dir()
 
     with open(os.path.join(dotlink_dir, "dotlinks.json"), "r") as f:
-        dotlinks = json.load(f) 
+        dotlinks = json.load(f)
         new_dotlinks = dotlinks.copy()
 
-    if os.path.lexists(link_name) and to_generic_home_path(link_name) in dotlinks.values():
-        for x in dotlinks.items(): 
-            if x[1] == to_generic_home_path(link_name):
+    if to_generic_home_path(link_name) in map(lambda x: x["path"], dotlinks.values()):
+        if os.path.exists(link_name):
+            os.remove(link_name)
+        else:
+            print("dotlink: " + link_name + ": No such file or directory" 
+                  "\nOnly removing link records in ", os.path.join(dotlink_dir, "dotlinks.json"))
+
+        for x in dotlinks.items():
+            if x[1]["path"] == to_generic_home_path(link_name):
                 new_dotlinks.pop(x[0], None)
 
-        os.remove(link_name)
+        with open(os.path.join(dotlink_dir, "dotlinks.json"), "w") as f:
+            json.dump(new_dotlinks, f)
     else:
-        print("dotlink: Unable to remove link")
-
-    with open(os.path.join(dotlink_dir, "dotlinks.json"), "w") as f:
-        json.dump(new_dotlinks, f)
+        print("dotlink: Unable to remove link: Link not found in ", os.path.join(dotlink_dir, "dotlinks.json"))
 
