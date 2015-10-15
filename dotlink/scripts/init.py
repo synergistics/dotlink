6fdd0a569f7b9fb22eaa1d47ebf864f8eeb85307
synergistics <kingjak678@gmail.com>
2015-10-15 09:25:37 -0400

    Adds decent documentation

--- dotlink/scripts/init.py
+++ dotlink/scripts/init.py
@@ -3,8 +3,15 @@ import json
 from dotlink.lib.pathmodifiers import to_generic_home_path, to_specific_path
 
 def init(command_relevants):
+    """Initialize a dotlink directory"""
+    
+    # Location of dotlink directory
     path = to_specific_path(command_relevants["<path>"] or ".")
+    
+    # Location of dotlinks.json
     json_path = os.path.join(path, "dotlinks.json")
+    
+    # Location of .dotlinkrc
     dotlinkrc = os.path.join(os.environ["HOME"], ".dotlinkrc")
 
     # If directory exists, nothing happens to it
@@ -15,6 +22,7 @@ def init(command_relevants):
         with open(json_path, "w") as f:
             json.dump({}, f)
     
+    # Identify location of dotlink dir
     # Will have to change once more can be added to dotlinkrc
     with open(dotlinkrc, "w") as f:
         f.write("dotlink_dir = " + to_generic_home_path(path))
