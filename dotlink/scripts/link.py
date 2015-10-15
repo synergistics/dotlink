42ef525285ce1a3832d7c0643b95b5b3508596ab
synergistics <kingjak678@gmail.com>
2015-10-15 09:10:08 -0400

    Removes developer docs. Don't be lazy.

--- dotlink/scripts/link.py
+++ dotlink/scripts/link.py
@@ -1,22 +1,3 @@
-"""
-# Developer Documentation: 'link' command
-Note: This is NOT user documentation
-
-# Description
-Links or symlinks a file in the dotlink directory to another file in the user's
-filesystem. 
-
-# Walkthrough steps
-1. Get target path, link_name, and -s switch variables
-2. Open dotlinks.json to be used as dict object
-3. Add key to dotlinks dict for the relative path of 'target' in the dotlink directory
-4. Set the value of said key to a dict containing path (string) and symbolic (boolean)
-keys
-..- Path is set to the generic home path of the link_name
-5. Attempt link or symlink (corresonding to the symbolic boolean) and write dotlinks
-back to dotlinks.json if successful
-
-"""
 import os
 import sys
 import json
